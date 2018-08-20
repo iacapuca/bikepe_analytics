7ca17c7a18274a2f48db1f5c73b51c6e495dbfe5
Pedro <iacapuca@gmail.com>
2018-08-20 18:56:21 -0300

    Filled route column with addRoute() results

--- trips_analytics/trips-util.py
+++ trips_analytics/trips-util.py
@@ -12,14 +12,16 @@ trips_df = pd.read_csv("trips.csv", encoding = 'utf8')
 trips_df.set_index('duration_seconds')
 
 def addRoute(start_station,end_station):
-    origin = str(start_station[0])+','+str(start_station[1])
-    destination = str(end_station[0])+','+str(end_station[1])
+    #origin = str(start_station[0])+','+str(start_station[1])
+    #print(origin)
+    #destination = str(end_station[0])+','+str(end_station[1])
     travelMode = 'bicycling'
-    payload = {'origin':origin,'destination':destination,'mode':travelMode,'key':key}
+    payload = {'origin':start_station,'destination':end_station,'mode':travelMode,'key':key}
     r = requests.get(endpoint, params=payload)
-    print(r.url)
     route_poly = (polyline.decode((r.json())['routes'][0]['overview_polyline']['points']))
+    time.sleep(1)
     return route_poly
 
-print(addRoute((-8.0625979,-34.872696),(-8.0625979,-34.872696)))
-#trips_df['route'] = trips_df.apply(addRoute(trips_df['start_station'],trips_df['end_station']))
+trips_df['route'] = trips_df.apply(lambda row: addRoute(row['start_station'], row['end_station']), axis=1)
+
+#trips_df.to_csv("trips_route.csv", encoding='utf-8', sep=',' , index=False)
