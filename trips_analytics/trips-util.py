8e5d8299abde7c89bd82fb54492b7fcb09505876
Pedro <iacapuca@gmail.com>
2018-08-20 17:59:48 -0300

    Fixed lat/lng tuple

--- trips_analytics/trips-util.py
+++ trips_analytics/trips-util.py
@@ -12,16 +12,14 @@ trips_df = pd.read_csv("trips.csv", encoding = 'utf8')
 trips_df.set_index('duration_seconds')
 
 def addRoute(start_station,end_station):
-    origin = str(start_station[0])
-    destination = str(end_station[0])
+    origin = str(start_station[0])+','+str(start_station[1])
+    destination = str(end_station[0])+','+str(end_station[1])
     travelMode = 'bicycling'
     payload = {'origin':origin,'destination':destination,'mode':travelMode,'key':key}
-    print(payload)
     r = requests.get(endpoint, params=payload)
-    route = polyline.decode((r.json())['routes'][0]['overview_polyline']['points'])
-    print("Finalizando envio")
-    time.sleep(5)
-    return route
+    print(r.url)
+    route_poly = (polyline.decode((r.json())['routes'][0]['overview_polyline']['points']))
+    return route_poly
 
-#addRoute((-8.0327102,-34.8967708),(-8.0625979,-34.872696))
-trips_df['route'] = trips_df.apply(addRoute(trips_df['start_station'],trips_df['end_station']))
+print(addRoute((-8.0625979,-34.872696),(-8.0625979,-34.872696)))
+#trips_df['route'] = trips_df.apply(addRoute(trips_df['start_station'],trips_df['end_station']))
